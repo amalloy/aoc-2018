db325472c33592dd35eac5b42eb1be90a18e84bd
Alan Malloy <alan@malloys.org>
2018-11-30 22:45:58 -0800

    Day 1, part 2 (N^2)

--- aoc-day01/src/Main.hs
+++ aoc-day01/src/Main.hs
@@ -8,14 +8,21 @@ parse ('+':x) = read x
 -- part1 [] = 0
 -- part1 (x:xs) = parse x + part1 xs
 
+firstDup :: Eq a => [a] -> Maybe a
+firstDup = go []
+  where go seen [] = Nothing
+        go seen (x:xs) | x `elem` seen = Just x
+                       | otherwise = go (x:seen) xs
+
 part1 :: [String] -> Int
 part1 = sum . map parse
 
-ints :: [Int]
-ints = 1:2:3:[]
+part2 :: [String] -> Maybe Int
+part2 = firstDup . scanl (+) 0 . cycle . map parse
 
 main :: IO ()
 main = do
   file <- getContents
   let contents = lines file
   print (part1 contents)
+  print (part2 contents)
