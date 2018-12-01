632042db06917bc66314df1e5b700eb64dbe1478
Alan Malloy <alan@malloys.org>
2018-11-30 22:20:50 -0800

    Day 1, part 1 (simple version)

--- aoc-day01/src/Main.hs
+++ aoc-day01/src/Main.hs
@@ -1,5 +1,18 @@
 module Main where
 
+parse :: String -> Int
+parse ('-':x) = negate (read x)
+parse ('+':x) = read x
+
+part1 :: [String] -> Int
+part1 [] = 0
+part1 (x:xs) = parse x + part1 xs
+
+ints :: [Int]
+ints = 1:2:3:[]
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  file <- getContents
+  let contents = lines file
+  print (part1 contents)
