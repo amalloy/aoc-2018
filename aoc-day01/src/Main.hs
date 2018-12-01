a87d172622474273d148c6e6d2df8540be649527
Alan Malloy <alan@malloys.org>
2018-11-30 22:26:49 -0800

    Day 1, part 1 (point-free version)

--- aoc-day01/src/Main.hs
+++ aoc-day01/src/Main.hs
@@ -4,9 +4,12 @@ parse :: String -> Int
 parse ('-':x) = negate (read x)
 parse ('+':x) = read x
 
+-- part1 :: [String] -> Int
+-- part1 [] = 0
+-- part1 (x:xs) = parse x + part1 xs
+
 part1 :: [String] -> Int
-part1 [] = 0
-part1 (x:xs) = parse x + part1 xs
+part1 = sum . map parse
 
 ints :: [Int]
 ints = 1:2:3:[]
