e3b6c0d86a26b82399a952b715fde65b7d8231f1
Alan Malloy <alan@malloys.org>
2018-12-01 21:42:40 -0800

    Day 2, part 1

--- aoc-day02/app/Main.hs
+++ aoc-day02/app/Main.hs
@@ -1,14 +1,23 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import qualified Data.Map as M
 
 type Input = [String]
 
+frequencies :: Ord a => [a] -> M.Map a Int
+frequencies = go M.empty
+  where go m [] = m
+        go m (x:xs) = go (M.insertWith (+) x 1 m) xs
+
 part1 :: Input -> Int
-part1 = undefined
+part1 words = let freqs = map (M.toList . frequencies) words
+                  has n = length . filter (any ((== n) . snd)) $ freqs
+              in has 2 * has 3
+
 
 part2 :: Input -> Int
-part2 = undefined
+part2 x = 0
 
 parse :: String -> Input
 parse = lines
