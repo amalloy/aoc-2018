9be3467087fe02d39fdeaeae59360abf9011e76f
Alan Malloy <alan@malloys.org>
2018-12-03 22:53:03 -0800

    Day 4, part 1: solution

--- aoc-day04/app/Main.hs
+++ aoc-day04/app/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Control.Arrow ((&&&))
 import Control.Applicative ((<|>))
+import Data.List (sortBy, foldl', maximumBy)
+import qualified Data.Map as M
+import Data.Ord (comparing)
 import Text.Parsec.Char (spaces, digit, char, string)
 import Text.Parsec.String (Parser)
 import Text.Parsec (many1, parse, ParseError)
@@ -46,8 +49,27 @@ sleep = string "falls asleep" *> pure Sleep
 wake :: Parser Event
 wake = string "wakes up" *> pure Wake
 
-part1 :: Input -> Input
-part1 = id
+sleepTimes :: [Entry] -> [(GuardId, Instant)]
+sleepTimes = go 0
+  where go _ [] = []
+        go _ (Entry _ (NewGuard id):xs) = go id xs
+        go g (Entry start Sleep : Entry end Wake:xs) =
+          let (Instant m d h _) = start
+              more = go g xs
+          in [(g, Instant m d h t) | t <- [minute start..minute end - 1]] ++ more
+
+frequencies :: Ord k => [k] -> M.Map k Int
+frequencies = foldl' (\m x -> M.insertWith (+) x 1 m) M.empty
+
+part1 :: Input -> Int
+part1 entries = let minutes = fmap (fmap minute) (sleepTimes entries)
+                    sleepiness = frequencies (map fst minutes)
+                    (id, _) = mostCommon sleepiness
+                    napTime = frequencies . map snd . filter ((== id) . fst) $ minutes
+                    (min, _) = mostCommon napTime
+                in id * min
+  where mostCommon = maximumBy (comparing snd) . M.toList
+
 
 part2 :: Input -> Int
 part2 = undefined
@@ -55,4 +77,6 @@ part2 = undefined
 
 
 main :: IO ()
-main = interact $ show . fmap (part1 &&& part2) . traverse (parse entry "stdin") . lines
+main = interact $ show . fmap (part1 &&& part2) .
+  fmap (sortBy (comparing timestamp)) .
+  traverse (parse entry "stdin") . lines
