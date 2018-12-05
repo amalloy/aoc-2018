d47d00dc9dff24874bd2616cca03cc5c0fee9421
Alan Malloy <alan@malloys.org>
2018-12-04 21:28:55 -0800

    Day 5, part 1

--- aoc-day05/app/Main.hs
+++ aoc-day05/app/Main.hs
@@ -1,17 +1,32 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Control.Monad (forever)
+import Data.Char (toUpper, isLetter)
+import Data.Function (fix)
 
-type Input = [String]
+
+type Input = String
+
+reactsWith :: Char -> Char -> Bool
+reactsWith a b = a /= b && (a == toUpper b || b == toUpper a)
+
+next :: Input -> Input
+next x@[_] = x
+next (x:y:xs) | x `reactsWith` y = next xs
+              | otherwise = x : next (y:xs)
+
+collapse :: Input -> Input
+collapse s | s == s' = s
+           | otherwise = collapse s'
+  where s' = next s
+
 
 part1 :: Input -> Int
-part1 = undefined
+part1 = length . collapse
 
 part2 :: Input -> Int
 part2 = undefined
 
-parse :: String -> Input
-parse = lines
-
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . parse
+main = interact $ show . (part1 &&& part2) . filter isLetter
