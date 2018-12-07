209a9b9774e93de8ed63aaa29a54d855d2b9e1de
Alan Malloy <alan@malloys.org>
2018-12-06 22:40:10 -0800

    Day 7, part 1

--- aoc-day07/app/Main.hs
+++ aoc-day07/app/Main.hs
@@ -1,17 +1,35 @@
 module Main where
 
+import Text.Regex.Applicative (string, anySym, (=~))
+import Data.List (minimum)
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import Control.Arrow ((&&&))
+import Control.Monad (guard)
+data Rule = Rule {prereq, goal :: Char} deriving Show
+type RuleSet = M.Map Char (S.Set Char)
 
-type Input = [String]
+part1 :: RuleSet -> String
+part1 = go S.empty
+  where go done rules | M.null rules = ""
+                      | otherwise = c : go (S.insert c done) (M.delete c rules)
+          where c = minimum $ do
+                  (goal, prereqs) <- M.assocs rules
+                  guard $ prereqs `S.isSubsetOf` done
+                  pure goal
 
-part1 :: Input -> Int
-part1 = undefined
+part2 :: RuleSet -> Int
+part2 = const 0
 
-part2 :: Input -> Int
-part2 = undefined
+parse :: String -> Maybe Rule
+parse = (=~ regex)
+  where regex = Rule <$> (string "Step " *> anySym)
+                     <*> (string " must be finished before step " *> anySym <* string " can begin.")
 
-parse :: String -> Input
-parse = lines
+ruleGraph :: [Rule] -> RuleSet
+ruleGraph rules = graph `M.union` goals
+  where graph = M.fromListWith S.union [(goal rule, S.singleton (prereq rule)) | rule <- rules]
+        goals = M.fromList [(prereq rule, S.empty) | rule <- rules]
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . parse
+main = interact $ show . fmap (part1 &&& part2) . fmap ruleGraph . traverse parse . lines
