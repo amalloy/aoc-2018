9a5e94d7757dec76154bd0b9696345b751925a6e
Alan Malloy <alan@malloys.org>
2018-12-08 01:03:17 -0800

    Day 8, part 1

--- aoc-day08/app/Main.hs
+++ aoc-day08/app/Main.hs
@@ -1,17 +1,33 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Control.Monad (replicateM)
+import Text.Parsec.Char (spaces, digit, char)
+import Text.Parsec.String (Parser)
+import Text.Parsec (parse, many1, sepEndBy1)
 
-type Input = [String]
+data Node = Node {metadata :: [Int], children :: [Node]} deriving Show
+
+int :: Parser Int
+int = read <$> many1 digit
+
+node :: Parser Node
+node = do
+  numChildren <- int
+  spaces
+  numMeta <- int
+  children <- replicateM numChildren (spaces *> node)
+  meta <- replicateM numMeta (spaces *> int)
+  pure $ Node meta children
+
+type Input = Node
 
 part1 :: Input -> Int
-part1 = undefined
+part1 (Node meta children) = sum meta + sum (map part1 children)
 
 part2 :: Input -> Int
 part2 = undefined
 
-parse :: String -> Input
-parse = lines
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . parse
+main = interact $ show . fmap (part1 &&& part2) . parse node "stdin"
