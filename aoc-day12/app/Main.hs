d980fe6c8091e32f5e4874eef8167e754933f3b8
Alan Malloy <alan@malloys.org>
2018-12-11 21:56:29 -0800

    Day 12, part 1

--- aoc-day12/app/Main.hs
+++ aoc-day12/app/Main.hs
@@ -1,17 +1,56 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.List (tails, transpose)
+import qualified Data.Map as M
 
-type Input = [String]
+data Input = Input [Indexed Bool] Transition deriving Show
+
+type Five a = (a,a,a,a,a)
+type Rule a = (Five a, a)
+data Indexed a = Ix {ix :: Int, v :: a} deriving (Show, Functor)
+type Transition = M.Map (Five Bool) Bool
+
+on :: Char -> Bool
+on = (== '#')
 
 part1 :: Input -> Int
-part1 = undefined
+part1 (Input init r) = let finalState = iterate (step r) init !! 20
+                       in sum [i | Ix i True <- finalState]
 
 part2 :: Input -> Int
-part2 = undefined
+part2 s = 0
+
+window :: Int -> [a] -> [[a]]
+window n = takeWhile ((== n) . length) . transpose . take n . tails
+
+stub :: Int -> Int -> a -> [Indexed a]
+stub from to x = [Ix i x | i <- [from..to]]
+
+step :: Transition -> [Indexed Bool] -> [Indexed Bool]
+step r xs = let ((Ix i _):more) = xs
+                (Ix j _) = last xs
+                prev = stub (i-4) (i-1) False
+                    <> xs
+                    <> stub (j+1) (j+4) False
+                in map advance . window 5 $ prev
+  where advance neighborhood@[_,_,focus,_,_] = next <$ focus
+          where [a,b,c,d,e] = map v neighborhood
+                next = r M.! (a,b,c,d,e)
+
+parseRule :: String -> Rule Bool
+parseRule s = let [input,_,[output]] = words s
+                  [a,b,c,d,e] = map on input
+              in ((a,b,c,d,e), on output)
 
 parse :: String -> Input
-parse = lines
+parse s = let (header:_:rules) = lines s
+              ["initial","state:",pots] = words header
+              initState = zipWith Ix [0..] . map on $ pots
+              transition = M.fromList . map parseRule $ rules
+          in Input initState transition
+
 
 main :: IO ()
 main = interact $ show . (part1 &&& part2) . parse
