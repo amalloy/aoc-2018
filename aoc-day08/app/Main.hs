06016124ed3faebe4b0dcf3678c2ef9f5f9c95d1
Alan Malloy <alan@malloys.org>
2018-12-18 12:16:49 -0800

    Day 8: try out recursion-schemes

--- aoc-day08/app/Main.hs
+++ aoc-day08/app/Main.hs
@@ -2,14 +2,20 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Main where
 
+import Control.Applicative (liftA2)
 import Control.Arrow ((&&&))
 import Control.Monad (replicateM)
+import Data.Function (on)
+import Data.Functor.Foldable (Fix(..), cata)
 import qualified Data.IntMap.Lazy as M
 import Text.Parsec.Char (spaces, digit, char)
 import Text.Parsec.String (Parser)
 import Text.Parsec (parse, many1, sepEndBy1)
 
-data Node a = Node {children :: [Node a], metadata :: [a]} deriving (Show, Functor, Foldable)
+data NodeF a b = NodeF {children :: [b], meta :: [a]} deriving (Show, Functor)
+type Node a = Fix (NodeF a)
+
+mkNode x y = Fix (NodeF x y)
 
 int :: Parser Int
 int = read <$> many1 digit
@@ -19,17 +25,18 @@ node = do
   numChildren <- int
   spaces
   numMeta <- int
-  Node <$> replicateM numChildren (spaces *> node) <*> replicateM numMeta (spaces *> int)
+  mkNode <$> replicateM numChildren (spaces *> node) <*> replicateM numMeta (spaces *> int)
 
 type Input = Node Int
 
 part1 :: Input -> Int
-part1 = sum
+part1 = cata (liftA2 ((+) `on` sum) children meta)
 
 part2 :: Input -> Int
-part2 (Node [] m) = sum m
-part2 (Node cs m) = let childValues = M.fromList . zip [1..] . map part2 $ cs
-                    in sum . map (flip (M.findWithDefault 0) childValues) $ m
+part2 = cata go
+  where go (NodeF [] m) = sum m
+        go (NodeF cs m) = let childValues = M.fromList . zip [1..] $ cs
+                          in sum . map (flip (M.findWithDefault 0) childValues) $ m
 
 main :: IO ()
 main = interact $ show . fmap (part1 &&& part2) . parse node "stdin"
