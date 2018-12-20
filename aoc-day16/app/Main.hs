097c49ed6e4bf6b16aa726d73d94b46e137fa63f
Alan Malloy <alan@malloys.org>
2018-12-19 16:36:52 -0800

    Day 16, part 1

--- aoc-day16/app/Main.hs
+++ aoc-day16/app/Main.hs
@@ -1,23 +1,32 @@
 module Main where
 
+import Control.Applicative (many, some)
 import Control.Arrow ((&&&))
 import Data.Bits ((.&.), (.|.))
 import Data.Bool (bool)
+import Data.Char (isDigit, isSpace)
 import Data.Function (on)
 import qualified Data.IntMap.Strict as M
+import Debug.Trace
+import Text.Regex.Applicative (sym, psym, string, match, RE)
 
-type Input = [String]
+type Input = ([Sample], [Instruction])
 
 type Argument = Int
-data Arguments = Arguments {in1, in2, out :: Argument}
-data ArgType = Register | Immediate
+data Arguments = Arguments {in1, in2, out :: Argument} deriving Show
+type Opcode = Int
+data Instruction = Instruction {opcode :: Opcode, arguments :: Arguments} deriving Show
+data ArgType = Register | Immediate deriving Show
 type RegIndex = Int
 type RegVal = Int
 type Registers = M.IntMap RegVal
 type Binary a = Int -> Int -> a
 type Operation = Arguments -> Registers -> Int
+type OpName = String
 
-ops :: [(String, Operation)]
+data Sample = Sample {before, after :: Registers, instruction :: Instruction} deriving Show
+
+ops :: [(OpName, Operation)]
 ops = concat [do (name, f) <- [("add", (+)), ("mul", (*)),
                                ("ban", (.&.)), ("bor", (.|.))]
                  (arg, suffix) <- suffixes
@@ -42,14 +51,32 @@ ops = concat [do (name, f) <- [("add", (+)), ("mul", (*)),
 apply :: Operation -> Arguments -> Registers -> Registers
 apply op args regs = M.insert (out args) (op args regs) regs
 
+interpretations :: Sample -> [OpName]
+interpretations (Sample before after (Instruction _ args)) =
+  [name | (name, op) <- ops, apply op args before == after]
+
+
 part1 :: Input -> Int
-part1 = const 0
+part1 = length . filter ((>= 3) . length) . map interpretations . fst
 
 part2 :: Input -> Int
 part2 = const 0
 
-parse :: String -> Input
-parse = lines
+parse :: String -> Maybe Input
+parse = match r
+  where r = (,) <$> (spaces `between` sample) <*> (spaces *> (spaces `between` instr) <* spaces)
+
+spaces = many (psym isSpace)
+sample = (\b i a -> Sample b a i) <$> regs "Before" <*> instr <*> regs "After"
+regs s = mkSample <$> (string s *> sym ':' *> spaces *> intList <* spaces)
+mkSample = M.fromList . zip [0..]
+intList = sym '[' *> (sym ',' *> spaces) `between` int <* sym ']'
+int = read <$> some (psym isDigit)
+between sep item = (:) <$> item <*> many (sep *> item)
+instr = Instruction <$> int <* spaces <*> args <* spaces
+args = collect <$> spaces `between` int
+collect [a,b,c] = Arguments a b c
+collect xs = traceShow xs $ undefined
 
 main :: IO ()
-main = interact $ show . (part1 &&& part2) . parse
+main = interact $ show . fmap (part1 &&& part2) . parse
